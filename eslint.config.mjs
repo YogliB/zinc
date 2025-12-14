b133a0fd3f095959e61709e21e60e16398616549
YogliB <yogev.boaronben-har@cyberark.com>
2025-12-14 11:42:44 +0200

    Update project configuration files and add Storybook setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 // @ts-check
 
 import eslint from '@eslint/js';
@@ -10,7 +13,12 @@ import preact from 'eslint-config-preact';
 
 export default defineConfig(
 	{
-		ignores: ['eslint.config.mjs', 'vite.config.ts', 'vitest.config.ts'],
+		ignores: [
+			'eslint.config.mjs',
+			'vite.config.ts',
+			'vitest.config.ts',
+			'**/.storybook/**',
+		],
 	},
 	eslint.configs.recommended,
 	tseslint.configs.strict,
